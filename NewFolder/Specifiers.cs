3ebfdceb24414f6e7cc54224cc68e8821ee1f936
realme <realme@SURAJARMAN>
2022-10-14 16:13:37 +0530

    Add project files.

---
+++ NewFolder/Specifiers.cs
@@ -0,0 +1,24 @@
+﻿using Firstcproject.NewFolder1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firstcproject.NewFolder1;
+namespace Project2.NewFolder
+{
+    public class Specifiers: Class1AccessSpecifiers
+    {
+        static void Main()
+        {
+
+            Specifiers sp = new Specifiers();
+
+
+            sp.Test1();
+            sp.Test2();
+            sp.Test3();
+
+        }
+    }
+}
